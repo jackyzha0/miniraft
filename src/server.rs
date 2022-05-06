87041283f44a9fc9c7598a025781a63e06d7ec6d
Jacky Zhao <j.zhao2k19@gmail.com>
2022-05-05 21:46:54 -0400

    revert duration, use ticks

--- src/server.rs
+++ src/server.rs
@@ -1,7 +1,4 @@
-use std::{
-    collections::{BTreeMap, BTreeSet},
-    time::{Duration, Instant},
-};
+use std::collections::{BTreeMap, BTreeSet};
 
 use crate::log::{Log, LogIndex};
 
@@ -11,19 +8,21 @@ pub type Term = u64;
 /// Type alias for the ID of a single Raft server
 pub type ServerId = usize;
 
+type Ticks = u32;
+
 /// Configuration options for a Raft server
 #[derive(Debug)]
 pub struct RaftConfig {
     /// How long a server should wait for a message from
     /// current leader before giving up and starting an election
-    pub election_timeout: Duration,
+    pub election_timeout: Ticks,
 
     /// How much random jitter to add to [`election_timeout`](Self::election_timeout)
-    pub election_timeout_jitter: Duration,
+    pub election_timeout_jitter: Ticks,
 
     /// How often a leader should send empty 'heartbeat' AppendEntry RPC
     /// calls to maintain power. Generally one magnitude smaller than [`election_timeout`](Self::election_timeout)
-    pub heartbeat_interval: Duration,
+    pub heartbeat_interval: Ticks,
 }
 
 /// A Raft server that replicates Logs of type `T`
@@ -65,28 +64,32 @@ pub enum RaftLeadershipState {
     Leader(LeaderState),
 }
 
+impl RaftLeadershipState {
+    fn tick(&mut self) {}
+}
+
 #[derive(Debug)]
 pub struct FollowerState {
     /// Current leader node is following
     leader: Option<ServerId>,
-    /// Future time to start an election if not reset by activity/heartbeat
-    election_time: Instant,
+    /// Ticks left to start an election if not reset by activity/heartbeat
+    election_time: Ticks,
 }
 
 #[derive(Debug)]
 pub struct CandidateState {
     /// Set of all nodes this node has received votes for
     votes_received: BTreeSet<ServerId>,
-    /// Future time to start an election if quorum is not reached
-    election_time: Instant,
+    /// Ticks left to start an election if quorum is not reached
+    election_time: Ticks,
 }
 
 #[derive(Debug)]
 pub struct LeaderState {
     /// Track state about followers to figure out what to send them next
     followers: BTreeMap<ServerId, NodeReplicationState>,
-    /// When to send the next heartbeat
-    heartbeat_timeout: Instant,
+    /// Ticks left till when to send the next heartbeat
+    heartbeat_timeout: Ticks,
 }
 
 /// State of a single Node as tracked by a leader
